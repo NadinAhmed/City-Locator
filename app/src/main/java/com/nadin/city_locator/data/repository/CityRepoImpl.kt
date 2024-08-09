e89a5139293569e59b5c8e086d807ecdd8e7305a
Nadin Ahmed <nadinahmed316@gmail.com>
2024-08-09 08:24:32 +0300

    Fix Sorting Casing

--- app/src/main/java/com/nadin/city_locator/data/repository/CityRepoImpl.kt
+++ app/src/main/java/com/nadin/city_locator/data/repository/CityRepoImpl.kt
@@ -1,5 +1,7 @@
 package com.nadin.city_locator.data.repository
 
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toLowerCase
 import com.nadin.city_locator.data.datasource.JsonCity
 import com.nadin.city_locator.domain.model.City
 import com.nadin.city_locator.domain.repository.CityRepo
@@ -12,7 +14,7 @@ class CityRepoImpl @Inject constructor(private val jsonCity: JsonCity) : CityRep
         return withContext(Dispatchers.IO) {
             val jsonString = jsonCity.readJsonFromAssets()
             val parsedCity = jsonCity.parseJsonToModel(jsonString)
-            parsedCity.map { it.toDomainModel() }.sortedBy { it.name }
+            parsedCity.map { it.toDomainModel() }.sortedBy { it.name.toLowerCase(Locale.current) }
         }
     }
 }
--- app/src/main/java/com/nadin/city_locator/presentation/cityList/composables/CityListItem.kt
+++ app/src/main/java/com/nadin/city_locator/presentation/cityList/composables/CityListItem.kt
@@ -1,5 +1,6 @@
 package com.nadin.city_locator.presentation.cityList.composables
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,68 +31,61 @@ import com.nadin.city_locator.presentation.theme.CityLocatorTheme
 @Composable
 fun CityListItem(city: City, onClick: () -> Unit, modifier: Modifier = Modifier) {
 
-    Box(
-        modifier = Modifier
-            .background(
-                brush = Brush.horizontalGradient(
-                    colors = listOf(
-                        MaterialTheme.colorScheme.primary,
-                        MaterialTheme.colorScheme.secondary
-                    )
+    Card(
+        onClick = onClick,
+        shape = RoundedCornerShape(size = 6.dp),
+        modifier = modifier
+            .fillMaxWidth(),
+        border = BorderStroke(
+            3.dp, brush = Brush.linearGradient(
+                colors = listOf(
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.secondary
                 )
             )
-            .padding(4.dp)
-    ) {
-        Card(
-            onClick = onClick,
-            shape = RoundedCornerShape(size = 6.dp),
-            modifier = modifier
-                .fillMaxWidth()
+        )
 
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 12.dp)
         ) {
-            Column(
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.Start,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 12.dp)
+            Text(
+                text = "${city.name}, ${city.country}",
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
             ) {
                 Text(
-                    text = "${city.name}, ${city.country}",
-                    style = MaterialTheme.typography.titleMedium,
+                    text = "Lat: ",
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.W600,
                 )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start,
-                ) {
-                    Text(
-                        text = "Lon: ",
-                        style = MaterialTheme.typography.titleSmall,
-                        fontWeight = FontWeight.W600,
-                    )
-                    Text(
-                        text = "${city.coordinates.lon}",
-                        style = MaterialTheme.typography.titleSmall,
-                        fontWeight = FontWeight.W400,
-                    )
-                    Spacer(modifier = Modifier.width(20.dp))
-                    Text(
-                        text = "Lat: ",
-                        style = MaterialTheme.typography.titleSmall,
-                        fontWeight = FontWeight.W600,
-                    )
-                    Text(
-                        text = "${city.coordinates.lat}",
-                        style = MaterialTheme.typography.titleSmall,
-                        fontWeight = FontWeight.W400,
-                    )
-                }
+                Text(
+                    text = "${city.coordinates.lat}",
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.W400,
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                Text(
+                    text = "Lon: ",
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.W600,
+                )
+                Text(
+                    text = "${city.coordinates.lon}",
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.W400,
+                )
 
             }
-
         }
     }
-
 }
 
 @Preview
